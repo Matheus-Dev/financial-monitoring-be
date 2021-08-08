02c3dafd0ce9661a835a61bb58de463b7335fd60
Matheus Augusto <matheus.augusto@solucx.com.br>
2021-08-08 17:53:27 -0300

    add FinancialControl module

--- src/financial-control/financial-control.module.ts
+++ src/financial-control/financial-control.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FinancialControl } from '../entity/financial-control.entity';
 import { FinancialControlController } from './financial-control.controller';
+import { FinancialControlService } from './financial-control.service';
 
 @Module({
-  controllers: [FinancialControlController]
+    imports: [TypeOrmModule.forFeature([ FinancialControl ])],
+    controllers: [ FinancialControlController ],
+    providers: [ FinancialControlService ]
 })
 export class FinancialControlModule {}
