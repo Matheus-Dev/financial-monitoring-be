ccd1796e8dc632f7b58d8ce37a4081385affae1a
Matheus Augusto <matheus.augusto@solucx.com.br>
2021-08-08 17:50:00 -0300

    add Config and TypeOrm Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FinancialControlModule } from './financial-control/financial-control.module';
-import { MongoserviceService } from './services/mongo/mongoservice.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [FinancialControlModule],
-  controllers: [AppController],
-  providers: [AppService, MongoserviceService],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'mongodb',
+            url: process.env.MONGODB_CONNECTION_STRING,
+            entities: [`${__dirname}/**/*.entity.{ts,js}`],
+            synchronize: false,
+            ssl: false,
+            useUnifiedTopology: true,
+            logging: true,
+            useNewUrlParser: true
+        }),
+        FinancialControlModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
