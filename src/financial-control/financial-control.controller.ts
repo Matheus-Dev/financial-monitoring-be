833d6102187bb62a8d409a387b5b7748f52355eb
Matheus Augusto <matheus.augusto@solucx.com.br>
2021-08-08 17:53:11 -0300

    add FinancialControl controller

--- src/financial-control/financial-control.controller.ts
+++ src/financial-control/financial-control.controller.ts
@@ -1,4 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, DefaultValuePipe, Get, ParseIntPipe, Post, Query, UsePipes } from '@nestjs/common';
+import { IFinancialControlSearch } from 'src/interfaces/financial-control-search';
+import { FinancialControlPipe } from 'src/pipes/financial-control.pipe';
+import { FinancialControlService } from './financial-control.service';
 
 @Controller('financial-control')
-export class FinancialControlController {}
+export class FinancialControlController {
+
+    constructor(private readonly service: FinancialControlService){}
+
+    @UsePipes(new FinancialControlPipe())
+    @Get('search')
+    public getBySearch (
+        @Query() search: IFinancialControlSearch) {
+        return this.service.find(search)
+    }
+
+}
