055d40a58454200fc6909a5ff194339e39bca0ce
Matheus Augusto <matheus.augusto@solucx.com.br>
2021-08-08 17:51:45 -0300

    add FinancialControl constant

---
+++ src/constants/financial-control.constants.ts
@@ -0,0 +1,7 @@
+export class FinancialControlConstants {
+
+    static FINANCIAL_CONTROL_DEFAULT_PAGE = 1
+    static FINANCIAL_CONTROL_DEFAULT_LIMIT = 10
+    static FINANCIAL_CONTROL_DEFAULT_SEARCH_FIELD = 'name'
+
+}
